0cf84e4e0e944b80c5ee93325d0363c3f75d3dae
Minion Milu <Zahidhasan.milu@gmail.com>
2022-06-18 12:29:11 +0600

    slug details , urls and view

--- calapp/views.py
+++ calapp/views.py
@@ -1,27 +1,54 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def calculator(request):
-    try:
-        c = ''
-        num1 = ''
-        num2 = ''
-        operator = ''
-        if request.method == 'POST':
-            num1 =eval(request.POST.get('number1'))
-            num2 = eval(request.POST.get('number2'))
-            operator = request.POST.get('operator')
+    # try:
+    #     c = ''
+    #     num1 = ''
+    #     num2 = ''
+    #     operator = ''
+    #     if request.method == 'POST':
+    #         num1 =eval(request.POST.get('number1'))
+    #         num2 = eval(request.POST.get('number2'))
+    #         operator = request.POST.get('operator')
             
             
-            if operator == '+':
-                c = num1 + num2
-            if operator == '-':
-                c = num1 - num2
-            if operator ==  '*':
-                c = num1 * num2
-            if operator ==  '/':
-                c = num1 / num2
+    #         if operator == '+':
+    #             c = num1 + num2
+    #         if operator == '-':
+    #             c = num1 - num2
+    #         if operator ==  '*':
+    #             c = num1 * num2
+    #         if operator ==  '/':
+    #             c = num1 / num2
            
-    except:
-        c = 'invalid Operator'
-    return render(request,'index.html', context={'c': c, 'num1':num1, 'num2':num2, 'operator':operator})
+    # except:
+    #     c = 'invalid Operator'
+    # return render(request,'index.html', context={'c': c, 'num1':num1, 'num2':num2, 'operator':operator})
+    if request.method =='POST':
+        num1 = eval(request.POST.get('number1'))
+        num2 = eval(request.POST.get('number2'))
+        operator = request.POST.get('operator')
+        if operator == '+':
+            c = num1+num2
+        elif operator == '-':
+            c = num1-num2
+        elif operator == '*':
+            c = num1*num2
+        elif operator == '/':
+            c = num1/num2
+        else:
+            c = 'invalid Operator'
+        return render(request,'index.html', context={'c': c, 'num1':num1, 'num2':num2, 'operator':operator})
+    return render(request,'index.html')
+
+
+def intslug(request, a, b):
+    return HttpResponse("Hello Int slug ", a+b)
+
+def strslug(request, x, y):
+    return HttpResponse(x+y)
+
+def justslug(request,x,y,z):
+    return HttpResponse(x+y+z)
